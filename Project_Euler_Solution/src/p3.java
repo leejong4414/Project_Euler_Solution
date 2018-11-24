74451c7632c193b1f65f5dc1be26b62233f64e27
jongholee <jongholee@172.16.105.115>
2018-11-24 14:14:39 -0800

    p3

---
+++ Project_Euler_Solution/src/p3.java
@@ -0,0 +1,24 @@
+import java.math.BigInteger;
+import java.util.*;
+
+public class p3 {
+
+    /*
+     * The prime factors of 13195 are 5, 7, 13 and 29.
+     * 
+     * What is the largest prime factor of the number 600851475143 ?
+     */
+    public static void main(String [ ] args){
+        BigInteger a = new BigInteger("600851475143");
+        System.out.println(solution(a));
+
+    }
+    
+   /* Solving Though process
+    * 1. 
+    *     
+    */
+    public static BigInteger solution(BigInteger n){
+        return new BigInteger("600851475143");
+    }
+}
