c41ebae32714b0efe51299df8a00de5fc16db6a1
jongholee <jongholee@172.16.105.115>
2018-11-25 19:20:45 -0800

    q3

--- Project_Euler_Solution/src/p3.java
+++ Project_Euler_Solution/src/p3.java
@@ -9,16 +9,27 @@ public class p3 {
      * What is the largest prime factor of the number 600851475143 ?
      */
     public static void main(String [ ] args){
-        BigInteger a = new BigInteger("600851475143");
+        long a = 600851475143L;
         System.out.println(solution(a));
  
     }
     
    /* Solving Though process
-    * 1. 
-    *     
+    * 1. brute force and find the biggest prime number smaller then this number and divisible -> O(n)
+    * 2. use prime factor property. Keep dividing when find divisible number.
     */
-    public static BigInteger solution(BigInteger n){
-        return new BigInteger("600851475143");
+    public static long solution(long n){
+        long factor = -1;
+        for (int i = 2; i * i <= n; i++) {
+            if (n == 1) 
+                break;
+            if (n % i != 0) 
+                continue;
+            factor = i;
+            while (n % i == 0) {
+                n /= i;
+            }
+        }
+        return n == 1 ? factor : n;
     }
 }
